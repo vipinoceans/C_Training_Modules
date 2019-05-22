acf0350dafc1e636b561a48dd5136cc518e7c279
Vipin kamalasanan <vipzoceans@gmail.com>
2019-05-22 12:56:48 +0530

    s

---
+++ factorial.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+main()
+{
+
+    int timesrepeat, Invalue, OutValue, a , b, c=1;
+
+
+
+    printf("Enter a value to find factorial\n");
+    scanf("%d",&Invalue);
+
+    for(a=Invalue;a>=1;a--)
+
+
+    printf("%d\n",a);
+
+
+
+
+
+}
